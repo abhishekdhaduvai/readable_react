b29409a4195df9bba19f686a954169a3830a7c67
Abhishek Dhaduvai <abhishek.dhaduvai1@gmail.com>
2017-09-26 22:35:07 -0700

    added a dropdown to sort

---
+++ src/Components/Sort.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
+import { connect } from 'react-redux';
+import { changeSort } from '../actions';
+
+// Needed for onTouchTap
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+class Sort extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  changeSort = (sort) => {
+      this.handleRequestClose();
+      this.props.changeSort(sort);
+  }
+
+  render() {
+    return (
+      <span>
+
+        <span 
+            className="sort-dropdown"
+            onTouchTap={this.handleTouchTap}>
+            <i className="material-icons dropdown-content sort-text">{this.props.site.sortBy}</i>
+        </span>
+
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.handleRequestClose}
+        >
+          <Menu>
+            <MenuItem 
+                className="sort-menu-item"
+                primaryText="Score"
+                onClick={() => this.changeSort("Score")}/>
+            <MenuItem 
+                className="sort-menu-item"
+                primaryText="New"
+                onClick={() => this.changeSort("New")} />
+            <MenuItem 
+                className="sort-menu-item"
+                primaryText="Controversial"
+                onClick={() => this.changeSort("Controversial")} />
+          </Menu>
+
+        </Popover>
+      </span>
+    );
+  }
+}
+
+function mapStateToProps ({ site }) {
+  return {
+    site
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    changeSort: (sort) => dispatch(changeSort(sort))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Sort)
