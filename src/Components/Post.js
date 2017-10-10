8228f3df5f93e83fd9a5ce38e31b2f2c1320e3c7
Abhishek Dhaduvai <abhishek.dhaduvai1@gmail.com>
2017-10-09 23:46:39 -0700

    post: removed the delete button

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,47 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Votes from './Votes';
 import Timestamp from './Utils/Timestamp';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-
-import { connect } from 'react-redux';
-import { deletePost } from '../actions'
 
 class Post extends React.Component{
 
     PropTypes = {
         post: PropTypes.object.isRequired,
-        deletePost: PropTypes.func.isRequired
-    }
-
-    state = {
-        open: false
-    }
-
-    handleOpen = () => {
-        this.setState({open: true});
-    };
-
-    handleClose = () => {
-        this.setState({open: false});
-    };
-
-    confirmDelete = (id) => {
-        alert("Hello")
     }
 
     render() {
         const { post } = this.props;
-        const actions = [
-            <FlatButton
-                label="OK"
-                primary={true}
-                onClick={this.handleClose}
-            />
-        ];
         return (
             <div>
-                {post.deleted === false && (
+                {!post.deleted && (
                     <div className="post">
                         <Votes post={post} />
                         <div className="post-info">
@@ -55,38 +26,12 @@ class Post extends React.Component{
                             <div className="comments-button">
                                 <a href= {`comments/${post.id}`}>comments</a>
                             </div>
-                            <div className="delete-button">
-                                <a href="" onClick={(e) => this.confirmDelete(e)}>delete</a>
-                            </div>
                         </div>
                     </div>
                 )}
-
-                <Dialog
-                    actions={actions}
-                    modal={false}
-                    open={this.state.open}
-                    onRequestClose={this.handleClose}>
-                    Fill all required fields
-                </Dialog>
             </div>
         )
     }
 }
 
-function mapStateToProps () {
-  return {
-
-  }
-}
-
-function mapDispatchToProps (dispatch) {
-  return {
-    deletePost: (id) => dispatch(deletePost(id))
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Post)
+export default Post
